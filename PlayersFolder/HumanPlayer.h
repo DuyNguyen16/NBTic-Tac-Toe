20b17efbc5d503c564a3215a7cc1300304bb936a
DuyNguyen16 <khanhduyn161@gmail.com>
2024-09-03 19:39:17 +1000

    wdawd

--- PlayersFolder/HumanPlayer.h
+++ PlayersFolder/HumanPlayer.h
@@ -7,8 +7,30 @@ class HumanPlayer : public Player
 {
 public:
     HumanPlayer(int x) : Player(x) {};
+    bool getMove(int &x, int &y, TicTacToe *board) override;
 };
 
+bool HumanPlayer::getMove(int &smallBoardX, int &smallBoardY, TicTacToe *board)
+{
 
+    if (board->getNoOfMoves() >= 9)
+    {
+        return false;
+    }
+
+    do
+    {
+        cout << " Enter row position: ";
+        cin >> smallBoardX;
+        cout << " Enter column position: ";
+        cin >> smallBoardY;
+        cout << endl;
+
+    } while (!isValidMove(smallBoardX - 1, smallBoardY - 1, board));
+    // Enter move on current board
+    smallBoardX = smallBoardX - 1;
+    smallBoardY = smallBoardY - 1;
+    return true;
+};
 
 #endif
--- PlayersFolder/Player.h
+++ PlayersFolder/Player.h
@@ -13,7 +13,7 @@ public:
     {
         playerNumber = x;
     }
-    bool getMove(int &x, int &y, TicTacToe *board);
+    virtual bool getMove(int &x, int &y, TicTacToe *board) = 0;
     bool isValidMove(int x, int y, TicTacToe *board);
 
     void displayTurn();
@@ -21,28 +21,7 @@ public:
     int getPlayerNumber() {return playerNumber;}
 };
 
-bool Player::getMove(int &smallBoardX, int &smallBoardY, TicTacToe *board)
-{
 
-    if (board->getNoOfMoves() >= 9)
-    {
-        return false;
-    }
-
-    do
-    {
-        cout << " Enter row position: ";
-        cin >> smallBoardX;
-        cout << " Enter column position: ";
-        cin >> smallBoardY;
-        cout << endl;
-
-    } while (!isValidMove(smallBoardX - 1, smallBoardY - 1, board));
-    // Enter move on current board
-    smallBoardX = smallBoardX - 1;
-    smallBoardY = smallBoardY - 1;
-    return true;
-};
 
 // check if the move on a Tic Tac Toe is valid
 bool Player::isValidMove(int x, int y, TicTacToe *board)
